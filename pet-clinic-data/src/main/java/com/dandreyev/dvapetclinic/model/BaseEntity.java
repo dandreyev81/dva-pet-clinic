7db1e75eceff6009830909f953494e65ac54dd11
Dmitriy Andreyev <dandreyev@copyright.com>
2021-11-10 21:53:33 -0500

    Updated Base Entity with JPA

--- pet-clinic-data/src/main/java/com/dandreyev/dvapetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/dandreyev/dvapetclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package com.dandreyev.dvapetclinic.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
 
     public Long getId() {
