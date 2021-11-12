3c55dc75fc0a16cfcfde810c35b95b7634f71e4d
Dmitriy Andreyev <dandreyev@copyright.com>
2021-11-12 00:32:11 -0500

    Create JPA services

--- pet-clinic-data/src/main/java/com/dandreyev/dvapetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dandreyev/dvapetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.dandreyev.dvapetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
+
