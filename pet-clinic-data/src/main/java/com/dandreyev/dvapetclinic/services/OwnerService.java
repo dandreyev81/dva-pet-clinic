2b7fbecda7843893517599fe60ad27aaba3bd5f9
Dmitriy Andreyev <dandreyev@copyright.com>
2021-12-08 01:35:19 -0500

    Add Find Owner

--- pet-clinic-data/src/main/java/com/dandreyev/dvapetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/dandreyev/dvapetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.dandreyev.dvapetclinic.services;
 
 import com.dandreyev.dvapetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName (String lastName);
+
+    List<Owner> findAllByLastNameLike (String lastName);
 
 }
